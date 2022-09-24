4586f32a18dd9adc4fd4c860ff82dd49370ff87e
Michał Kukulski <bleidd04@gmail.com>
2022-09-24 13:38:04 +0200

    -adds timer

--- Assets/Scripts/Arcade/GameManager.cs
+++ Assets/Scripts/Arcade/GameManager.cs
@@ -3,14 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+
+    public float timeRemaining = 30;
+    public TMP_Text counterText;
+    public bool timerIsRunning = false;
+    private void Start()
+    {
+        // Starts the timer automatically
+        timerIsRunning = true;
+    }
+
     private void Update()
     {
         TryToRestart();
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+            DisplayTime(timeRemaining);
+        }
+        else
+        {
+            Debug.Log("Time has run out!");
+            timerIsRunning = false;
+        }
     }
 
     private void TryToRestart()
@@ -19,5 +40,12 @@ public class GameManager : MonoBehaviour
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        counterText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 }
