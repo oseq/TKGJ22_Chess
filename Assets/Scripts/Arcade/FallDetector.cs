ac1a8383b69beaa0ac4c75d82afd4d09ed3ea9d6
KJakubowski <konrad.mj08@gmail.com>
2022-09-24 15:10:06 +0200

    Added FallDetector

---
+++ Assets/Scripts/Arcade/FallDetector.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+public class FallDetector : MonoBehaviour
+{
+    public event Action<PlayerController> OnPlayerFelt = delegate { };
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var player = other.GetComponent<PlayerController>();
+
+        if (player)
+        {
+            OnPlayerFelt?.Invoke(player);
+        }
+    }
+}
