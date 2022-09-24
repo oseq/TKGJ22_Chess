dbd4661fa25acaf88915ffc542828d7d010176de
KJakubowski <konrad.mj08@gmail.com>
2022-09-24 17:15:11 +0200

    Added players action buttons view

---
+++ Assets/Scripts/Arcade/UI/ActionButtonView.cs
@@ -0,0 +1,28 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActionButtonView : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _cooldownContainer;
+    [SerializeField]
+    private Image _cooldownImage;
+    [SerializeField]
+    private TMP_Text _cooldownText;
+    [SerializeField]
+    private GameObject _readyContainer;
+
+    public void UpdateTimer(float timeLeft, float cooldownTime)
+    {
+        bool isReady = timeLeft <= 0f;
+        _cooldownContainer.gameObject.SetActive(!isReady);
+        _readyContainer.gameObject.SetActive(isReady);
+
+        if (!isReady)
+        {
+            _cooldownImage.fillAmount = timeLeft / cooldownTime;
+            _cooldownText.text = timeLeft.ToString();
+        }
+    }
+}
