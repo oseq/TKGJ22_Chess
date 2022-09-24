5c7f26aecb2f82ef4bb6703205a6c98453da244e
Jakub Stokowski <jakubstokowski@tutanota.com>
2022-09-24 17:11:21 +0200

    Implement ForceFieldPowerUp

--- Assets/Scripts/PowerUp/ForceFieldPowerUp.cs
+++ Assets/Scripts/PowerUp/ForceFieldPowerUp.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 [CreateAssetMenu(fileName="NewForceFielPowerUp")]
 public class ForceFieldPowerUp : IPowerUpAction
 {
-    public float range;
+    public float radius;
     public float force;
 
     public override void Perform(IPowerUpAction.Context context)
     {
-        Debug.Log("Boom!");
+        var position = context.instigator.transform.position;
+        Collider[] colliders = Physics.OverlapSphere(position, radius);
+
+        foreach (Collider hit in colliders)
+        {
+            if (hit.gameObject != context.instigator)
+            {
+                Rigidbody rb = hit.GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    rb.AddExplosionForce(force, position, radius, 3.0F);
+                }
+            }
+        }
     }
 
-    public override void Detached(Context context)
-    {
-        
-    }
+    public override void Detached(Context context) {}
 }
