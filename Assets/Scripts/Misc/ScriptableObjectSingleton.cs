8cfe7fd468bfa53dc488dd75c5bedd15fbbfc778
KJakubowski <konrad.mj08@gmail.com>
2022-09-24 21:17:29 +0200

    Added singleton classes

---
+++ Assets/Scripts/Misc/ScriptableObjectSingleton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+public class ScriptableObjectSingleton<T> : ScriptableObject where T : ScriptableObject
+{
+    private static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                T[] objects = Resources.LoadAll<T>(string.Empty);
+                if (objects.Length > 1)
+                {
+                    throw new InvalidOperationException();
+                }
+                else
+                {
+                    instance = objects.FirstOrDefault();
+                }
+            }
+            return instance;
+        }
+    }
+}
