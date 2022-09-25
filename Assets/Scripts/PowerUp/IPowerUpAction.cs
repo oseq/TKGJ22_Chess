c329a1bb0720090c4b1c49317acc038c9ae32568
Mateusz Kmak <hamsterkmak@gmail.com>
2022-09-25 11:15:23 +0200

    permanent trail for speed boost

--- Assets/Scripts/PowerUp/IPowerUpAction.cs
+++ Assets/Scripts/PowerUp/IPowerUpAction.cs
@@ -7,19 +7,19 @@ public abstract class IPowerUpAction : ScriptableObject
     {
         public GameObject instigator;
         public ParticleSystem effect;
-        public TrailRendererEnabler onAttachTrail;
+        public TrailRendererToggler onAttachTrail;
     }
 
     public abstract void Perform(Context context);
     public abstract void Detached(Context context);
 }
 
-public class TrailRendererEnabler
+public class TrailRendererToggler
 {
     private TrailRenderer _trailRenderer;
     private float _time;
 
-    public TrailRendererEnabler(TrailRenderer trailRenderer, float time)
+    public TrailRendererToggler(TrailRenderer trailRenderer, float time)
     {
         _trailRenderer = trailRenderer;
         _time = time;
@@ -32,5 +32,31 @@ public class TrailRendererEnabler
             _trailRenderer.emitting = true;
             DOVirtual.DelayedCall(_time, () => _trailRenderer.emitting = false);
         }
+    }
+    
+    public void TurnOnTrailPermanently()
+    {
+        if (_trailRenderer != null)
+        {
+            _trailRenderer.emitting = true;
+        }
+    }
+
+    public void TurnOffTrail()
+    {
+        if (_trailRenderer != null)
+        {
+            _trailRenderer.emitting = false;
+        }
+    }
+
+    public bool isToogled()
+    {
+        if (_trailRenderer != null)
+        {
+            return _trailRenderer.emitting;
+        }
+
+        return false;
     }
 }
